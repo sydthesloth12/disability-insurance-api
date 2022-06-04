7072b7cb0112a9751c39df130a15b754bd81a04d
Sydney <sydneysirois17@gmail.com>
2022-06-04 16:11:38 -0400

    employer, and employee routes added

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const { indexRender, catchAll } = require('./controllers/disabilityIns')
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.set("view engine", "pug");
+
+app.get('/', indexRender);
+
+app.all('*', catchAll);
+
+app.listen(1337, () => {
+  // eslint-disable-next-line no-console
+  console.log("Listening on http://localhost:1337");
+});
